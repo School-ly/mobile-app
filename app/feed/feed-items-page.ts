cbab255cfa22dd1f8bfb13a94fccfe9d0e776c61
Alex Tsvetanov <14363901+Alex-Tsvetanov@users.noreply.github.com>
2020-03-04 17:14:15 -0800

    BACKEND CONNECTION

--- app/feed/feed-items-page.ts
+++ app/feed/feed-items-page.ts
@@ -1,13 +1,33 @@
 import { View } from "tns-core-modules/ui/core/view";
 import { ItemEventData } from "tns-core-modules/ui/list-view";
-import { NavigatedData, Page } from "tns-core-modules/ui/page";
+import { NavigatedData, Page, Observable, EventData, PropertyChangeData } from "tns-core-modules/ui/page";
 
 import { FeedViewModel } from "./feed-view-model";
 import { Item } from "./shared/item";
+import { getString} from "tns-core-modules/http";
 
 export function onNavigatingTo(args: NavigatedData) {
     const page = <Page>args.object;
-    page.bindingContext = new FeedViewModel();
+    const vm = new FeedViewModel();
+    
+        const index = Math.round(Math.random() * 1000) % 2;
+        var str = index.toString();
+        if (index == 0) str = "";
+        getString("https://raw.githubusercontent.com/School-ly/server/master/data" + str + ".json").then((r: string) => {
+            vm.set('items', JSON.parse(r));
+            console.log(r);
+        }, (e) => {
+            // >> (hide)
+            console.log("Error: ");
+            console.log(e);
+            // << (hide)
+        });
+    
+        vm.on("propertyChange", (args: PropertyChangeData) => {
+            vm.notify(args);
+        });
+
+    page.bindingContext = vm;
 }
 
 export function onItemTap(args: ItemEventData) {
