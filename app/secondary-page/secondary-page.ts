c1a702588d9f1d5d1c5703660649d389250212ec
Alex Tsvetanov <alex_tsvetanov_2002@abv.bg>
2020-03-05 03:31:05 -0800

    finally working the real-time fetching

---
+++ app/secondary-page/secondary-page.ts
@@ -0,0 +1,9 @@
+import { Page } from "tns-core-modules/ui/page";
+import { EventData, fromObject } from "tns-core-modules/data/observable";
+
+export function onNavigatedTo(args: EventData) {
+    let page = <Page>args.object;
+    page.bindingContext = fromObject({
+
+    });
+}
